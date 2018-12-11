d5d0576a3b865a70ee4a8e5a7ac701c3831c40f2
Chris <chrismcrow@gmail.com>
2018-12-11 13:32:24 -0800

    Updates

---
+++ precache-manifest.47e42214782e454366d27cb6e8afac11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76fdc5049c2ed996a2c2c132dc0b409a",
+    "url": "/ultimateTicTacToe-CreateReactApp/static/media/Space-Desktop.76fdc504.jpg"
+  },
+  {
+    "revision": "a37603467aeec38d7017",
+    "url": "/ultimateTicTacToe-CreateReactApp/static/js/runtime~main.a3760346.js"
+  },
+  {
+    "revision": "8df937281ac8509f1d3c",
+    "url": "/ultimateTicTacToe-CreateReactApp/static/js/main.8df93728.chunk.js"
+  },
+  {
+    "revision": "7d2d7afef0c723059ed4",
+    "url": "/ultimateTicTacToe-CreateReactApp/static/js/1.7d2d7afe.chunk.js"
+  },
+  {
+    "revision": "8df937281ac8509f1d3c",
+    "url": "/ultimateTicTacToe-CreateReactApp/static/css/main.0265fba5.chunk.css"
+  },
+  {
+    "revision": "7d2d7afef0c723059ed4",
+    "url": "/ultimateTicTacToe-CreateReactApp/static/css/1.d7a0c74b.chunk.css"
+  },
+  {
+    "revision": "517225c1dee2f512ce77aeeb01ef9263",
+    "url": "/ultimateTicTacToe-CreateReactApp/index.html"
+  }
+];
