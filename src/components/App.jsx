ce501f303fffa91ad6c6e8f84fd83278ba28356b
Chris <chrismcrow@gmail.com>
2018-12-11 12:58:20 -0800

    get project working in create-react-app

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import GlobalBoard from './GlobalBoard';
+import Header from './Header';
+import Status from './Status';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Status />
+      <GlobalBoard />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
 
+const render = (Component) => {
+    ReactDOM.render(
+        <HashRouter>
+          <Provider store={store}>
+            <Component/>
+          </Provider>
+        </HashRouter>,
+      document.getElementById('root')
+    );
+  };
+  
+  render(App);
+  
+  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
